1b9324712a4de854ff711383bcdd4d433a6e353b
MarceloVilchs <m.vilchess@duocuc.cl>
2024-04-08 21:31:04 -0400

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import BarraNav from './componentes/BarraNav';
 
 function App() {
+  
+  //let num = 24;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Market Free</h1>
+      <BarraNav></BarraNav>
     </div>
   );
 }
---
+++ src/componentes/BarraNav.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function BarraNav() {
+    return (
+        <div>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit, totam officia esse eos labore adipisci facilis necessitatibus deserunt architecto quas nemo sunt. Officia nemo optio id consectetur repellendus illum voluptas!
+            Provident, atque magnam, tempore sed rem facilis, illo temporibus possimus accusamus veritatis delectus iure magni pariatur. Voluptates blanditiis placeat praesentium maxime ipsa quis libero molestias asperiores doloribus dignissimos, provident porro.
+            Ullam esse cum ducimus, est, nisi saepe dolorum nihil odio et doloremque veniam, nemo voluptatum facilis itaque! Quasi neque sit itaque nihil veniam illo? Accusamus animi in dolores reiciendis adipisci.
+            Repellat magni reprehenderit unde. Quis obcaecati vitae minus corporis et nesciunt numquam facere dolore. Officiis debitis, tempora quo, aut possimus eum a eos quae hic voluptatum repudiandae ad amet facere.
+        </div>
+    )
+}
+
+export default BarraNav;    
